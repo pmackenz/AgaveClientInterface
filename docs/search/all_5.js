409924293d971f0a175b97ee4f03df8b48df8d14
Peter Sempolinski <psempoli@users.noreply.github.com>
2018-10-24 02:15:55 -0400

    Added some doxygen comments

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['remotedatainterface',['RemoteDataInterface',['../classRemoteDataInterface.html',1,'']]],
+  ['remotedatainterface',['RemoteDataInterface',['../classRemoteDataInterface.html',1,'RemoteDataInterface'],['../classRemoteDataInterface.html#a55062db895a74286f5c0d57ec3f024a8',1,'RemoteDataInterface::RemoteDataInterface()']]],
   ['remotedatareply',['RemoteDataReply',['../classRemoteDataReply.html',1,'']]],
   ['remotefiletree',['RemoteFileTree',['../classRemoteFileTree.html',1,'']]],
   ['remotejobdata',['RemoteJobData',['../classRemoteJobData.html',1,'']]],
